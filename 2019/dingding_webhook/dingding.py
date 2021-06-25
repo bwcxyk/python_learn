f090a7c0502ec4587264a9de9eb7a27fc4650f5c
不忘初心 <1624717079@qq.com>
2021-06-25 15:49:47 +0800

    2021-6-25

--- 2019/dingding_webhook/dingding.py
+++ 2019/dingding_webhook/dingding.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python
-#coding:utf-8
-#zabbix钉钉报警
-import requests,json,sys,os,datetime
-webhook="https://oapi.dingtalk.com/robot/send?access_token=0d2f" #说明：这里改为自己创建的机器人的token值
-user=sys.argv[1]
-text=sys.argv[3]
-data={
+# coding:utf-8
+# zabbix钉钉报警
+import datetime
+import json
+import os
+import requests
+import sys
+
+webhook = "https://oapi.dingtalk.com/robot/send?access_token=0d2f"  # 说明：这里改为自己创建的机器人的token值
+user = sys.argv[1]
+text = sys.argv[3]
+data = {
     "msgtype": "text",
     "text": {
         "content": text
     },
     "at": {
         "atMobiles": [
-            user #需要@群里谁
+            user  # 需要@群里谁
         ],
-        "isAtAll": False #是否全部@，True为是,False为否
+        "isAtAll": False  # 是否全部@，True为是,False为否
     }
 }
 headers = {'Content-Type': 'application/json'}
-x=requests.post(url=webhook,data=json.dumps(data),headers=headers)
+x = requests.post(url=webhook, data=json.dumps(data), headers=headers)
 if os.path.exists("/usr/local/zabbix/logs/dingding.log"):
-    f=open("/usr/local/zabbix/logs/dingding.log","a+")
+    f = open("/usr/local/zabbix/logs/dingding.log", "a+")
 else:
-    f=open("/usr/local/zabbix/logs/dingding.log","w+")
-f.write("\n"+"--"*30)
+    f = open("/usr/local/zabbix/logs/dingding.log", "w+")
+f.write("\n" + "--" * 30)
 if x.json()["errcode"] == 0:
-    f.write("\n"+str(datetime.datetime.now())+"    "+str(user)+"    "+"发送成功"+"\n"+str(text))
+    f.write("\n" + str(datetime.datetime.now()) + "    " + str(user) + "    " + "发送成功" + "\n" + str(text))
     f.close()
 else:
-    f.write("\n"+str(datetime.datetime.now()) + "    " + str(user) + "    " + "发送失败" + "\n" + str(text))
+    f.write("\n" + str(datetime.datetime.now()) + "    " + str(user) + "    " + "发送失败" + "\n" + str(text))
     f.close()
