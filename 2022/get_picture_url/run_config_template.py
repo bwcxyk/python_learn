3a4bd1d54f297d148264bda7d49a51d726d32cec
不忘初心 <1624717079@qq.com>
2022-09-27 14:52:24 +0800

    get_picture_url

---
+++ 2022/get_picture_url/run_config_template.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+
+# 配置文件，此文件为 run_config.py 文件的模板，请修改下面的配置内容后，将本文件名修改为 run_config.py 即可运行生效
+
+# oracle 连接字符串，如果端口号为默认的 1521，则 :端口号 的部分也可以省略
+oracle_connect_string = "用户名/密码@连接到的IP地址:端口号/连接到的数据库"
+
+
+# 保存到的文件夹， ./ 表示保存到 python 脚本所在文件夹下
+save_folder = "./"
