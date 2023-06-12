e5f563a8980beb2dd0eb4072537fa7f1937f08b0
不忘初心 <1624717079@qq.com>
2023-06-12 11:23:20 +0800

    优化

--- 2023/aliyun/security_group.py
+++ 2023/aliyun/security_group.py
@@ -21,67 +21,76 @@ access_secret = os.getenv('access_secret')
 region_id = os.getenv('region_id')
 sg_id = os.getenv('sg_id')
 
-# 打印环境变量的值，验证读取是否正确
-print(access_key)
-print(access_secret)
-print(region_id)
-print(sg_id)
-
 # 连接阿里云
 client = AcsClient(access_key, access_secret, region_id)
-# 获取当前的公网IP
-ip = requests.get("http://ip.42.pl/raw").text.strip()
 
-# 读取上一次的公网 IP
-if os.path.exists("ip.txt"):
-    with open("ip.txt", "r") as f:
-        old_ip = f.read().strip()
-else:
-    old_ip = ""
 
-# 如果公网 IP 改变了，删除旧的授权规则
-if ip != old_ip:
-    # 获取指定的安全组
-    sg_request = DescribeSecurityGroupsRequest.DescribeSecurityGroupsRequest()
-    sg_request.set_SecurityGroupId(sg_id)
-    response = client.do_action_with_exception(sg_request)
-    security_group = json.loads(response)['SecurityGroups']['SecurityGroup'][0]
-
+def update_security_group():
+    # 获取当前的公网IP
+    ip = requests.get("http://ip.42.pl/raw").text.strip()
     # 需要授权的端口列表
     port_list = [22, 1521, 3306, 5432, 6379]
 
-    # 遍历需要授权的端口列表，给安全组添加授权规则
-    for port in port_list:
-        # 删除已有的授权规则
-        revoke_request = RevokeSecurityGroupRequest()
-        revoke_request.set_SecurityGroupId(sg_id)
-        revoke_request.set_IpProtocol('tcp')
-        revoke_request.set_PortRange(f'{port}/{port}')
-        revoke_request.set_SourceCidrIp(f'{old_ip}/32')
-        print(f"删除安全组的规则: port={port}, SourceCidrIp={old_ip}")
-        try:
-            client.do_action_with_exception(revoke_request)
-            print(f"端口{port}删除成功")
-        except Exception as e:
-            print(f"端口{port}删除失败: {e}")
+    # 获取指定的安全组
+    sg_request = DescribeSecurityGroupsRequest.DescribeSecurityGroupsRequest()
+    sg_request.set_SecurityGroupId(sg_id)
+    try:
+        response = client.do_action_with_exception(sg_request)
+        security_group = json.loads(response)['SecurityGroups']['SecurityGroup'][0]
+        # 打印安全组的名称
+        print(f"Security group: {security_group['SecurityGroupName']}")
+    except Exception as e:
+        print(f"Failed to get security group with ID '{sg_id}': {e}")
+        security_group = None
+    # 如果 security_group 为空，则退出程序
+    if security_group is None:
+        exit()
 
-        # 添加新的授权规则
-        authorize_request = AuthorizeSecurityGroupRequest.AuthorizeSecurityGroupRequest()
-        authorize_request.set_IpProtocol('tcp')
-        authorize_request.set_PortRange(f'{port}/{port}')
-        authorize_request.set_SourceCidrIp(f'{ip}/32')
-        authorize_request.set_SecurityGroupId(sg_id)
-        print(f"添加安全组规则：允许来自 IP {ip} 的请求访问端口 {port}")
-        try:
-            client.do_action_with_exception(authorize_request)
-            print(f"端口{port}授权成功")
-        except Exception as e:
-            print(f"端口{port}授权失败: {e}")
+    # 读取上一次的公网 IP
+    if os.path.exists("ip.txt"):
+        with open("ip.txt", "r") as f:
+            old_ip = f.read().strip()
+    else:
+        old_ip = ""
 
-    # 将新的公网 IP 写入文件
-    with open("ip.txt", "w") as f:
-        f.write(ip)
+    # 如果公网 IP 改变了，删除旧的授权规则
+    if ip != old_ip:
+        # 遍历需要授权的端口列表，给安全组添加授权规则
+        for port in port_list:
+            # 删除已有的授权规则
+            revoke_request = RevokeSecurityGroupRequest()
+            revoke_request.set_SecurityGroupId(sg_id)
+            revoke_request.set_IpProtocol('tcp')
+            revoke_request.set_PortRange(f'{port}/{port}')
+            revoke_request.set_SourceCidrIp(f'{old_ip}/32')
+            print(f"删除安全组的规则: port={port}, SourceCidrIp={old_ip}")
+            try:
+                client.do_action_with_exception(revoke_request)
+                print(f"端口{port}删除成功")
+            except Exception as e:
+                print(f"端口{port}删除失败: {e}")
 
-    print("安全组授权更新成功。")
-else:
-    print("公网IP未改变，无需更新安全组授权。")
+            # 添加新的授权规则
+            authorize_request = AuthorizeSecurityGroupRequest.AuthorizeSecurityGroupRequest()
+            authorize_request.set_IpProtocol('tcp')
+            authorize_request.set_PortRange(f'{port}/{port}')
+            authorize_request.set_SourceCidrIp(f'{ip}/32')
+            authorize_request.set_SecurityGroupId(sg_id)
+            print(f"添加安全组规则：允许来自 IP {ip} 的请求访问端口 {port}")
+            try:
+                client.do_action_with_exception(authorize_request)
+                print(f"端口{port}授权成功")
+            except Exception as e:
+                print(f"端口{port}授权失败: {e}")
+
+        # 将新的公网 IP 写入文件
+        with open("ip.txt", "w") as f:
+            f.write(ip)
+
+        print("安全组授权更新成功。")
+    else:
+        print("公网IP未改变，无需更新安全组授权。")
+
+
+if __name__ == '__main__':
+    update_security_group()
